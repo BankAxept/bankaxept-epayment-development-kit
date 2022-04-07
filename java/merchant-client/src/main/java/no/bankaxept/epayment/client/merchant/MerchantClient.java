afdd138b8fdbe9f63cafb58554d3686633c09350
Aleksander Andresen <aleksander.andresen@vipps.no>
2022-04-07 14:20:51 +0200

    Remove redundant classes

--- java/merchant-client/src/main/java/no/bankaxept/epayment/client/merchant/MerchantClient.java
+++ java/merchant-client/src/main/java/no/bankaxept/epayment/client/merchant/MerchantClient.java
@@ -4,16 +4,17 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import no.bankaxept.epayment.client.base.BaseClient;
-import no.bankaxept.epayment.client.base.AbstractClient;
-import no.bankaxept.epayment.client.base.ResponseStatus;
-import no.bankaxept.epayment.client.base.SinglePublisher;
+import no.bankaxept.epayment.client.base.*;
+import no.bankaxept.epayment.client.base.http.HttpResponse;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Flow;
 
-public class MerchantClient extends AbstractClient {
+public class MerchantClient {
+
+    private final BaseClient baseClient;
+
 
     private final static String PAYMENTS_URL = "/payments";
 
@@ -24,15 +25,15 @@ public class MerchantClient extends AbstractClient {
     private final Executor executor = Executors.newSingleThreadExecutor();
 
     public MerchantClient(BaseClient baseClient) {
-        super(baseClient);
+        this.baseClient = baseClient;
     }
 
     public MerchantClient(String baseurl, String apimKey, String username, String password) {
-        super(baseurl, apimKey, username, password);
+        this.baseClient = new BaseClient(baseurl, apimKey, username, password);
     }
 
     public Flow.Publisher<ResponseStatus> payment(PaymentRequest request, String correlationId) throws JsonProcessingException {
-        return postEmptyResponseBody(PAYMENTS_URL, new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId);
+        return new MapOperator<>(baseClient.post(PAYMENTS_URL, new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
     }
 
 }
