e75ae19d571deb91fde18d6f7e5f636d8a71ba9a
Aleksander Andresen <aleksander.andresen@bidbax.no>
2022-05-20 11:54:16 +0200

    Add constructor

--- java/merchant-client/src/main/java/no/bankaxept/epayment/client/merchant/SimulationPaymentRequest.java
+++ java/merchant-client/src/main/java/no/bankaxept/epayment/client/merchant/SimulationPaymentRequest.java
@@ -2,6 +2,7 @@ package no.bankaxept.epayment.client.merchant;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,6 +10,25 @@ import java.util.List;
 public class SimulationPaymentRequest extends PaymentRequest implements SimulationRequest {
 
     private List<String> simulationValues = new ArrayList<>();
+
+    public SimulationPaymentRequest(PaymentRequest paymentRequest) {
+        this.inStore(paymentRequest.isInStore());
+        this.amount(paymentRequest.getAmount());
+        this.amountBreakdown(paymentRequest.getAmountBreakdown());
+        this.automaticCapture(paymentRequest.isAutomaticCapture());
+        this.encryptedCardholderAuthenticationData(paymentRequest.getEncryptedCardholderAuthenticationData());
+        this.merchantAggregatorId(paymentRequest.getMerchantAggregatorId());
+        this.merchantId(paymentRequest.getMerchantId());
+        this.merchantName(paymentRequest.getMerchantName());
+        this.merchantDisplayName(paymentRequest.getMerchantDisplayName());
+        this.merchantOrderReference(paymentRequest.getMerchantOrderReference());
+        this.merchantOrderMessage(paymentRequest.getMerchantOrderMessage());
+        this.merchantReference(paymentRequest.getMerchantReference());
+        this.messageId(paymentRequest.getMessageId());
+        this.inStore(paymentRequest.isInStore());
+        this.transactionTime(paymentRequest.getTransactionTime());
+
+    }
 
     public SimulationPaymentRequest simulationValue(String simulationValue) {
         this.simulationValues.add(simulationValue);
