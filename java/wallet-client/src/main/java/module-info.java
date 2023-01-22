6e31c6d4944604e4e3762077ec70ba1c2a4af0ff
Thomas Johansen <3448852+thxmasj@users.noreply.github.com>
2023-01-22 22:53:38 +0100

    wip

---
+++ java/wallet-client/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module no.bankaxept.epayment.sdk.merchant {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires no.bankaxept.epayment.sdk.baseclient;
+    requires swagger.annotations;
+    requires java.compiler;
+    requires jsr305;
+
+    exports no.bankaxept.epayment.client.wallet;
+}
