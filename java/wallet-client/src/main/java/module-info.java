02606ad7b1ef1b9e0f2abbc6878b5c24a4a67a73
Thomas Johansen <3448852+thxmasj@users.noreply.github.com>
2023-01-23 20:53:17 +0100

    wip

--- java/wallet-client/src/main/java/module-info.java
+++ java/wallet-client/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module no.bankaxept.epayment.sdk.merchant {
+module no.bankaxept.epayment.client.wallet {
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
@@ -7,5 +7,6 @@ module no.bankaxept.epayment.sdk.merchant {
     requires java.compiler;
     requires jsr305;
 
-    exports no.bankaxept.epayment.client.wallet;
+    exports no.bankaxept.epayment.client.wallet.outgoing;
+    exports no.bankaxept.epayment.client.wallet.incoming;
 }
--- java/wallet-client/src/main/java/no/bankaxept/epayment/client/wallet/WalletClient.java
+++ java/wallet-client/src/main/java/no/bankaxept/epayment/client/wallet/WalletClient.java
@@ -8,6 +8,8 @@ import no.bankaxept.epayment.client.base.BaseClient;
 import no.bankaxept.epayment.client.base.MapOperator;
 import no.bankaxept.epayment.client.base.RequestStatus;
 import no.bankaxept.epayment.client.base.SinglePublisher;
+import no.bankaxept.epayment.client.wallet.outgoing.EnrolCardRequest;
+import no.bankaxept.epayment.client.wallet.outgoing.PaymentRequest;
 
 import java.util.UUID;
 import java.util.concurrent.Executor;
@@ -23,23 +25,31 @@ public class WalletClient {
   private final Executor executor = Executors.newSingleThreadExecutor();
 
   public WalletClient(BaseClient baseClient) {
-      this.baseClient = baseClient;
+    this.baseClient = baseClient;
   }
 
   public WalletClient(String baseurl, String apimKey, String username, String password) {
-      this.baseClient = new BaseClient.Builder(baseurl).apimKey(apimKey).withScheduledToken(username, password).build();
+    this.baseClient = new BaseClient.Builder(baseurl).apimKey(apimKey).withScheduledToken(username, password).build();
   }
 
   public Flow.Publisher<RequestStatus> enrolCard(EnrolCardRequest request, String correlationId) {
-      try {
-          return new MapOperator<>(baseClient.post("/payment-tokens", new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
-      } catch (JsonProcessingException e) {
-          throw new RuntimeException(e);
-      }
+    try {
+      return new MapOperator<>(baseClient.post("/payment-tokens", new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   public Flow.Publisher<RequestStatus> deleteToken(UUID tokenId, String correlationId) {
-      return new MapOperator<>(baseClient.delete(String.format("/payment-tokens/%s", tokenId), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+    return new MapOperator<>(baseClient.delete(String.format("/payment-tokens/%s", tokenId), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+  }
+
+  public Flow.Publisher<RequestStatus> requestPayment(PaymentRequest request, String correlationId) {
+    try {
+      return new MapOperator<>(baseClient.post("/payments", new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
   }
 
 }
