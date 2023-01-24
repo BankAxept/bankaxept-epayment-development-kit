c15c1de324ee271c849230b5ce06252754ee43ad
Thomas Johansen <3448852+thxmasj@users.noreply.github.com>
2023-01-24 14:05:01 +0100

    The consolidated architecture has not defined the wallet api (#62)

---
+++ java/wallet-client/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module no.bankaxept.epayment.client.wallet {
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires no.bankaxept.epayment.sdk.baseclient;
+    requires swagger.annotations;
+    requires java.compiler;
+    requires jsr305;
+
+    exports no.bankaxept.epayment.client.wallet.outgoing;
+    exports no.bankaxept.epayment.client.wallet.incoming;
+}
---
+++ java/wallet-client/src/main/java/no/bankaxept/epayment/client/wallet/WalletClient.java
@@ -0,0 +1,55 @@
+package no.bankaxept.epayment.client.wallet;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import no.bankaxept.epayment.client.base.BaseClient;
+import no.bankaxept.epayment.client.base.MapOperator;
+import no.bankaxept.epayment.client.base.RequestStatus;
+import no.bankaxept.epayment.client.base.SinglePublisher;
+import no.bankaxept.epayment.client.wallet.outgoing.EnrolCardRequest;
+import no.bankaxept.epayment.client.wallet.outgoing.PaymentRequest;
+
+import java.util.UUID;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
+
+public class WalletClient {
+
+  private final BaseClient baseClient;
+
+  private final ObjectMapper objectMapper = new ObjectMapper().registerModule(new JavaTimeModule()).disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+  private final Executor executor = Executors.newSingleThreadExecutor();
+
+  public WalletClient(BaseClient baseClient) {
+    this.baseClient = baseClient;
+  }
+
+  public WalletClient(String baseurl, String apimKey, String username, String password) {
+    this.baseClient = new BaseClient.Builder(baseurl).apimKey(apimKey).withScheduledToken(username, password).build();
+  }
+
+  public Flow.Publisher<RequestStatus> enrolCard(EnrolCardRequest request, String correlationId) {
+    try {
+      return new MapOperator<>(baseClient.post("/payment-tokens", new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public Flow.Publisher<RequestStatus> deleteToken(UUID tokenId, String correlationId) {
+    return new MapOperator<>(baseClient.delete(String.format("/payment-tokens/%s", tokenId), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+  }
+
+  public Flow.Publisher<RequestStatus> requestPayment(PaymentRequest request, String correlationId) {
+    try {
+      return new MapOperator<>(baseClient.post("/payments", new SinglePublisher<>(objectMapper.writeValueAsString(request), executor), correlationId), httpResponse -> httpResponse.getStatus().toResponse());
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
