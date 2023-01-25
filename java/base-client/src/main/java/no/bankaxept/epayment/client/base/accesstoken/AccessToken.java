5ca11b7c5c48aeabb4565cf91f1c024a3b0da77b
Aleksander Andresen <aleksander.andresen@bidbax.no>
2023-01-25 12:59:30 +0100

    Adhere to oauth2 spec (#60)

--- java/base-client/src/main/java/no/bankaxept/epayment/client/base/accesstoken/AccessToken.java
+++ java/base-client/src/main/java/no/bankaxept/epayment/client/base/accesstoken/AccessToken.java
@@ -3,19 +3,28 @@ package no.bankaxept.epayment.client.base.accesstoken;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.regex.Pattern;
 
 class AccessToken {
     private final String token;
     private final Instant expiry;
 
-    public AccessToken(String token, Instant expiry) {
+    public AccessToken(String token, long expiresIn, Clock clock) {
         this.token = token;
-        this.expiry = expiry;
+        this.expiry = clock.instant().plusSeconds(expiresIn);
+    }
+
+    public AccessToken(String token, long expiresIn) {
+        this(token, expiresIn, Clock.systemDefaultZone());
     }
 
     static AccessToken parse(String input) {
         return Parser.parse(input);
+    }
+
+    static AccessToken parse(String input, Clock clock) {
+        return Parser.parse(input, clock);
     }
 
 
@@ -27,22 +36,26 @@ class AccessToken {
         return expiry;
     }
 
-    public long millisUntilTenMinutesBeforeExpiry(Clock clock) {
-        return Duration.between(clock.instant().plusSeconds(600), expiry).toMillis();
+    public long secondsUntilTenMinutesBeforeExpiry(Clock clock) {
+        Instant tenMinutesBeforeExpiry = expiry.minus(10, ChronoUnit.MINUTES);
+        return Duration.between(clock.instant(), tenMinutesBeforeExpiry).toSeconds();
     }
 
     private static class Parser {
-        private static final Pattern tokenPattern = Pattern.compile("\"accessToken\"\\s*:\\s*\"(.*)\"");
-        private static final Pattern expiryPattern = Pattern.compile("\"expiresOn\"\\s*:\\s*(\\d+)");
+        private static final Pattern tokenPattern = Pattern.compile("\"access_token\"\\s*:\\s*\"(.*)\"");
+        private static final Pattern expiryPattern = Pattern.compile("\"expires_in\"\\s*:\\s*(\\d+)");
 
         static AccessToken parse(String input) {
+            return parse(input, Clock.systemDefaultZone());
+        }
+
+        static AccessToken parse(String input, Clock clock) {
             var tokenMatcher = tokenPattern.matcher(input);
             var expiryMatcher = expiryPattern.matcher(input);
             if (!tokenMatcher.find() || !expiryMatcher.find()) {
-                throw new IllegalArgumentException("Could not parse token or expiry"); //onError
+                throw new IllegalArgumentException("Could not parse token or expiry");
             }
-            return new AccessToken(tokenMatcher.group(1), Instant.ofEpochMilli(Long.parseLong(expiryMatcher.group(1))));
+            return new AccessToken(tokenMatcher.group(1), Long.parseLong(expiryMatcher.group(1)), clock);
         }
-
     }
 }
