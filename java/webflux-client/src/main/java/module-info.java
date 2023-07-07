bc83d45ea22d4e8d714f4dc2af94808a7671a7a3
BankAxept ePayment Platform Pipeline <epp-platform@dev-azure.com>
2023-07-07 11:16:35 +0200

    Merchant client is unusable

--- java/webflux-client/src/main/java/module-info.java
+++ java/webflux-client/src/main/java/module-info.java
@@ -5,9 +5,5 @@ module no.bankaxept.epayment.sdk.webfluxclient {
   requires no.bankaxept.epayment.sdk.baseclient;
   requires spring.webflux;
   requires spring.web;
-  requires io.netty.resolver;
-  requires io.netty.handler;
-  requires reactor.netty.http;
-  requires reactor.netty.core;
   provides no.bankaxept.epayment.client.base.spi.HttpClientProvider with WebFluxHttpClientProvider;
 }
--- java/webflux-client/src/main/java/no/bankaxept/epayment/client/webflux/WebFluxClient.java
+++ java/webflux-client/src/main/java/no/bankaxept/epayment/client/webflux/WebFluxClient.java
@@ -1,8 +1,5 @@
 package no.bankaxept.epayment.client.webflux;
 
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
-import io.netty.resolver.DefaultAddressResolverGroup;
-import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Flow.Publisher;
@@ -10,14 +7,11 @@ import java.util.function.Function;
 import no.bankaxept.epayment.client.base.http.HttpClient;
 import no.bankaxept.epayment.client.base.http.HttpResponse;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.adapter.JdkFlowAdapter;
 import reactor.core.publisher.Mono;
-import reactor.netty.http.Http2SslContextSpec;
-import reactor.netty.http.HttpProtocol;
 
 public class WebFluxClient implements HttpClient {
 
@@ -25,16 +19,6 @@ public class WebFluxClient implements HttpClient {
 
   public WebFluxClient(String baseUrl) {
     webClient = WebClient.create(baseUrl);
-//    var httpClient = reactor.netty.http.client.HttpClient.create()
-//        .protocol(HttpProtocol.H2)
-//        .responseTimeout(Duration.ofSeconds(10))
-//        .resolver(DefaultAddressResolverGroup.INSTANCE)
-//        .secure(sslContextSpec -> sslContextSpec.sslContext(Http2SslContextSpec
-//            .forClient()
-//            .configure(sslContextBuilder -> sslContextBuilder
-//                //.protocols("TLSv1","TLSv1.1","TLSv1.2", "TLSv1.3")
-//                .trustManager(InsecureTrustManagerFactory.INSTANCE))));
-//    webClient = WebClient.builder().baseUrl(baseUrl).clientConnector(new ReactorClientHttpConnector(httpClient)).build();
   }
 
   public WebFluxClient() {
