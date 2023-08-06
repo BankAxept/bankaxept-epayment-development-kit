f0ced705c70dc261cb022493d04c14b1033c765f
Thomas Johansen <3448852+thxmasj@users.noreply.github.com>
2023-08-06 17:51:39 +0200

    Redundant dependency swagger-parser

--- java/merchant-client/src/main/java/module-info.java
+++ java/merchant-client/src/main/java/module-info.java
@@ -2,10 +2,8 @@ module no.bankaxept.epayment.sdk.merchant {
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
   requires com.fasterxml.jackson.datatype.jsr310;
+  requires jakarta.annotation;
   requires no.bankaxept.epayment.sdk.baseclient;
-  requires swagger.annotations;
-  requires java.compiler;
-  requires jsr305;
 
   exports no.bankaxept.epayment.client.merchant;
 }
