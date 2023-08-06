2106e81fbd42d93070e571137ab823c58fc0c5a7
BankAxept ePayment Platform Pipeline <epp-platform@dev-azure.com>
2023-08-06 16:01:47 +0200

    wip

--- java/wallet-client/src/main/java/module-info.java
+++ java/wallet-client/src/main/java/module-info.java
@@ -2,10 +2,8 @@ module no.bankaxept.epayment.client.wallet {
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
   requires com.fasterxml.jackson.datatype.jsr310;
+  requires jakarta.annotation;
   requires no.bankaxept.epayment.sdk.baseclient;
-  requires swagger.annotations;
-  requires java.compiler;
-  requires jsr305;
 
   exports no.bankaxept.epayment.client.wallet.partner;
   exports no.bankaxept.epayment.client.wallet.bankaxept;
