7dafcccf51800e1470c54864365d2a45e23eb8fd
Borys Zhalilo <borys.zhalilo@bidbax.no>
2023-08-25 13:59:49 +0200

    updated dns name to a new one: epp.stoetest.cloud -> api.epp.stoetest.cloud

--- java/client-test/src/test/java/no/bankaxept/epayment/client/test/merchant/MerchantClientIT.java
+++ java/client-test/src/test/java/no/bankaxept/epayment/client/test/merchant/MerchantClientIT.java
@@ -25,8 +25,8 @@ public class MerchantClientIT {
 
   private MerchantClient testClient() throws MalformedURLException {
     return new MerchantClient(
-        new URL("https://epp.stoetest.cloud/access-token/v1/accesstoken"),
-        new URL("https://epp.stoetest.cloud/merchant"),
+        new URL("https://api.epp.stoetest.cloud/access-token/v1/accesstoken"),
+        new URL("https://api.epp.stoetest.cloud/merchant"),
         System.getenv("CLIENT_ID"),
         System.getenv("CLIENT_SECRET")
     );
