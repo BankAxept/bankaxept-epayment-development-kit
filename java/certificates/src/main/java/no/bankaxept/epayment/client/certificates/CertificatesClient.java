4850b08cdf2eef85a7d26776e80c43f26b269ece
Mario Rojo <mario.rojo@bidbax.no>
2025-10-06 15:46:43 +0200

    Add client test merchant

---
+++ java/certificates/src/main/java/no/bankaxept/epayment/client/certificates/CertificatesClient.java
@@ -0,0 +1,42 @@
+package no.bankaxept.epayment.client.certificates;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow;
+import no.bankaxept.epayment.client.base.BaseClient;
+import no.bankaxept.epayment.client.base.MapOperator;
+import no.bankaxept.epayment.client.base.RequestStatus;
+import no.bankaxept.epayment.client.base.http.HttpResponse;
+
+public class CertificatesClient {
+
+  private final BaseClient baseClient;
+
+  public CertificatesClient(BaseClient baseClient) {
+    this.baseClient = baseClient;
+  }
+
+  private final Executor executor = Executors.newSingleThreadExecutor();
+
+  public CertificatesClient(URL authorizationServerUrl, URL resourceServerUrl, String clientId, String clientSecret) {
+    this(
+        new BaseClient.Builder(resourceServerUrl)
+            .withScheduledToken(authorizationServerUrl, clientId, clientSecret)
+            .build()
+    );
+  }
+
+  public Flow.Publisher<RequestStatus> getMerchantCertificates() {
+    var emptyHeaders = new HashMap<String, List<String>>();
+    return new MapOperator<>(
+        baseClient.get(
+            "/merchant",
+            emptyHeaders
+        ),
+        HttpResponse::requestStatus
+    );
+  }
+}
---
+++ java/client-test/src/test/java/no/bankaxept/epayment/client/test/certificates/CertificatesClientTest.java
@@ -0,0 +1,44 @@
+package no.bankaxept.epayment.client.test.certificates;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.ok;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
+
+import com.github.tomakehurst.wiremock.client.MappingBuilder;
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder;
+import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
+import com.github.tomakehurst.wiremock.matching.EqualToPattern;
+import java.net.MalformedURLException;
+import no.bankaxept.epayment.client.base.RequestStatus;
+import no.bankaxept.epayment.client.certificates.CertificatesClient;
+import no.bankaxept.epayment.client.test.AbstractBaseClientWireMockTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import reactor.adapter.JdkFlowAdapter;
+import reactor.test.StepVerifier;
+
+public class CertificatesClientTest extends AbstractBaseClientWireMockTest {
+
+  private CertificatesClient client;
+
+  @BeforeEach
+  public void setup(WireMockRuntimeInfo wmRuntimeInfo) throws MalformedURLException {
+    super.setup(wmRuntimeInfo);
+    client = new CertificatesClient(baseClient);
+  }
+
+  @Test
+  public void merchant_certs_successful() {
+    stubFor(MerchantEndpoint(ok()));
+    StepVerifier.create(JdkFlowAdapter.flowPublisherToFlux(client.getMerchantCertificates()))
+        .expectNext(RequestStatus.Accepted)
+        .verifyComplete();
+  }
+
+  private MappingBuilder MerchantEndpoint(ResponseDefinitionBuilder responseBuilder) {
+    return get(urlPathEqualTo("/merchant"))
+        .withHeader("Authorization", new EqualToPattern(bearerToken()))
+        .willReturn(responseBuilder);
+  }
+}
