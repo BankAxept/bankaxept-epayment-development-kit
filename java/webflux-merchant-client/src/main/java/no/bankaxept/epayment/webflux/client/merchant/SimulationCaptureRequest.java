0f6a0e0ccf09c9d6e2afbfa977d569fdb4bd547d
Mario Rojo <mario.rojo@bidbax.no>
2025-10-15 14:15:38 +0200

    Add webflux-merchant-client to devkit

---
+++ java/webflux-merchant-client/src/main/java/no/bankaxept/epayment/webflux/client/merchant/SimulationCaptureRequest.java
@@ -0,0 +1,27 @@
+package no.bankaxept.epayment.webflux.client.merchant;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
+import java.util.List;
+import no.bankaxept.epayment.client.base.SimulationRequest;
+
+@JsonIgnoreProperties(value = {"simulationValues"})
+public class SimulationCaptureRequest extends CaptureRequest implements SimulationRequest {
+
+  private final List<String> simulationValues = new ArrayList<>();
+
+  public SimulationCaptureRequest simulationValue(String simulationValue) {
+    this.simulationValues.add(simulationValue);
+    return this;
+  }
+
+  public SimulationCaptureRequest simulationValues(List<String> simulationValues) {
+    this.simulationValues.addAll(simulationValues);
+    return this;
+  }
+
+  public List<String> getSimulationValues() {
+    return simulationValues;
+  }
+
+}
---
+++ java/webflux-merchant-client/src/main/java/no/bankaxept/epayment/webflux/client/merchant/SimulationRefundRequest.java
@@ -0,0 +1,27 @@
+package no.bankaxept.epayment.webflux.client.merchant;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.ArrayList;
+import java.util.List;
+import no.bankaxept.epayment.client.base.SimulationRequest;
+
+@JsonIgnoreProperties(value = {"simulationValues"})
+public class SimulationRefundRequest extends RefundRequest implements SimulationRequest {
+
+  private final List<String> simulationValues = new ArrayList<>();
+
+  public SimulationRefundRequest simulationValue(String simulationValue) {
+    this.simulationValues.add(simulationValue);
+    return this;
+  }
+
+  public SimulationRefundRequest simulationValues(List<String> simulationValues) {
+    this.simulationValues.addAll(simulationValues);
+    return this;
+  }
+
+  public List<String> getSimulationValues() {
+    return simulationValues;
+  }
+
+}
