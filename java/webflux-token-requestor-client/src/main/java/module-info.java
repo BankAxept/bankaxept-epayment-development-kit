b583aaec8df32a1ae2cb690aa57201540c666bbd
Mario Rojo <mario.rojo@bidbax.no>
2025-10-15 15:05:05 +0200

    Add webflux-token-client to devkit

---
+++ java/webflux-token-requestor-client/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module no.bankaxept.epayment.sdk.tokenrequestor {
+  requires com.fasterxml.jackson.core;
+  requires com.fasterxml.jackson.databind;
+  requires com.fasterxml.jackson.datatype.jsr310;
+  requires jakarta.annotation;
+  requires no.bankaxept.epayment.sdk.baseclient;
+  requires reactor.core;
+
+  exports no.bankaxept.epayment.webflux.client.tokenrequestor.bankaxept;
+}
