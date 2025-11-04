b69082e3dbe0eaf24e07e8ed2a2d339b29894196
Mads Johannessen <mads.johannessen@bidbax.no>
2025-11-04 11:28:41 +0100

    chore: add conventional-pr GH action (#264)

---
+++ .github/commitlint.rules.js
@@ -0,0 +1,14 @@
+module.exports = {
+    rules: {
+        'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'deps']],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'never'],
+        'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+        'subject-empty': [2, 'never'],
+        'subject-full-stop': [2, 'never', '.'],
+        'header-max-length': [2, 'always', 100],
+        'body-leading-blank': [1, 'always'],
+        'footer-leading-blank': [1, 'always'],
+        'footer-max-line-length': [2, 'always', 100]
+    }
+};
